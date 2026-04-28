fe3fcdd3097796156c0cf8361645628832f2462c
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-28 11:05:07 -0400

    fixed return on toString method. display deposits runs

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -69,7 +69,7 @@ public class Transaction {
 //        date|time|description|vendor|amount
 //        2023-04-15|10:13:25|ergonomic keyboard|Amazon|-89.50
 //        2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00
-return  super.toString();
+return  getDate() + "|" + getTime() + "|" +  getVendor() + "|"+  getDescription() + "|"+ getAmount();
     }
 
 }
